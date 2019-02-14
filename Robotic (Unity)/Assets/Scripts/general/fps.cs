9d2b2354956719e047e60ca59772e1c2ae9c37d5
David Schäfer <divad.schaefer@gmail.com>
2019-02-14 17:10:22 +0100

    push

---
+++ Robotic (Unity)/Assets/Scripts/general/fps.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fps : MonoBehaviour
+{
+    public short framesPerSeconed;
+
+    private void Start()
+    {
+        Application.targetFrameRate = framesPerSeconed;
+    }
+}
