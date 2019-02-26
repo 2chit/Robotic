1bd1b24fc30d43cb74d69871c423a9243c3fd5a3
OnSebii <lang.sebastian.006@gmail.com>
2019-02-26 13:25:01 +0100

    Website Version 1.0

---
+++ Robotic (Unity)/Assets/Scripts/Tür/open_close.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open_close : MonoBehaviour
+{
+    public bool open;
+
+    
+
+    public FixedUpdate()
+    {
+        if (Input.GetKeyDown)
+        {
+
+        }
+    }
+}
