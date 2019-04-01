b9358b29ec21ed4427189f669666059aae71111e
David Schäfer <divad.schaefer@gmail.com>
2019-04-01 22:27:32 +0200

    Updated

--- Robotic (Unity)/Assets/Scripts/Enemie/enemieBrain.cs
+++ Robotic (Unity)/Assets/Scripts/Enemie/enemieBrain.cs
@@ -6,17 +6,23 @@ public class enemieBrain : MonoBehaviour
 {
     public bool standing;
     public bool lookingToSide; //true when right/ flase when left
+    public string lookingFor;
     public float canSeeTo;
 
-    public GameObject Weapon;
+    public GameObject weapon;
 
+    
     private void FixedUpdate()
     {
-        
+        RaycastHit2D hit = RayCast_();
+        if (hit.collider.gameObject.name == lookingFor)
+        {
+            weapon.GetComponent<weaponScript>().Shoot();
+        }
     }
 
-    private RaycastHit2D _RayCast(Vector2 raycastTo)
+    private RaycastHit2D RayCast_()
     {
-        return Physics2D.Raycast(new Vector2(this.transform.position.x * (lookingToSide == true ? 1 : -1), this.Weapon.transform.position.y), raycastTo, canSeeTo);
+        return Physics2D.Raycast(new Vector2(this.transform.position.x * (lookingToSide == true ? 1 : -1), this.weapon.transform.localScale.y * 1.1f + this.transform.localScale.y), new Vector2(this.transform.position.x * (lookingToSide == true ? 1 : -1) + (lookingToSide == true ? 1 : -1), this.weapon.transform.localScale.y * 1.1f + this.transform.localScale.y), canSeeTo);
     }
 }
