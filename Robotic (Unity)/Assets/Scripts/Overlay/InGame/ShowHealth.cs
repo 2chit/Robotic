07a6f83fdbcc59c12749fca8b59fa5efb27ce80f
David Schäfer <divad.schaefer@gmail.com>
2019-04-02 19:06:31 +0200

    updated player sripts and added healthbar

---
+++ Robotic (Unity)/Assets/Scripts/Overlay/InGame/ShowHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHealth : MonoBehaviour
+{
+    public GameObject player;
+    public Slider slider;
+
+    private void FixedUpdate()
+    {
+        slider.value = player.GetComponent<playerStats>().health;
+    }
+}
---
+++ Robotic (Unity)/Assets/Scripts/Player/playerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerStats : MonoBehaviour
+{
+    public short health;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "bullet")
+        {
+            this.health -= collision.gameObject.GetComponent<Bullet>().damage;
+            Debug.Log($"Health {this.health}");
+            if(this.health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
